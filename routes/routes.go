94643e2383f0b9be2ce96318004b343c4c504279
Jeferson <jeferson.neres@gmail.com>
2024-12-11 14:07:14 -0300

    Crud

--- routes/routes.go
+++ routes/routes.go
@@ -13,5 +13,8 @@ func HandleRequest() {
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/persanalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/persanalidades/{id}", controllers.RetornaUmaPersonalidade).Methods("Get")
+	r.HandleFunc("/api/persanalidades", controllers.CriaUmaNovaPersonalidade).Methods("Post")
+	r.HandleFunc("/api/persanalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
+	r.HandleFunc("/api/persanalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
